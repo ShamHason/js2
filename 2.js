15f73dd9f56ff35847360373b1f3f7cb934bbef7
ShamHason <shamhasonn@gmail.com>
2024-11-29 16:03:32 +0200

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,20 @@
+'use strict'
+//sham hason
+function isPrime(num){
+    if(num<2)
+    return false;
+for(let i=2;i<=Math.sqrt(num);i++) {		
+    if (num%i===0)       
+        return false;
+   }
+return true;
+}
+
+
+for(let i = 0 ; i<237;i++)
+    if(isPrime(i))
+        console.log(i);
+
+
+
+
