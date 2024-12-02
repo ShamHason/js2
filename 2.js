287d576a776d00c3792c5b5f796edbf4dd39a81c
ShamHason <shamhasonn@gmail.com>
2024-12-02 17:38:36 +0200

    2.js

--- 2.js
+++ 2.js
@@ -1,19 +1,23 @@
 'use strict'
-//sham hason
-function isPrime(num){
+//Sham Hason and Sofia Kononenko
+
+//this function determines if a number is a prime number
+
+function checkIsPrime(num){
+
     if(num<2)
     return false;
+
 for(let i=2;i<=Math.sqrt(num);i++) {		
-    if (num%i===0)       
+    if (num%i==0)       
         return false;
    }
 return true;
 }
 
-
-for(let i = 0 ; i<237;i++)
-    if(isPrime(i))
-        console.log(i);
+for (let i = 2; i < 237; i++) 
+    if (checkIsPrime(i)) 
+      console.log(i);  
 
 
 
